7b02958e764c3f4f05fa7399047aa3acbf6870e8
Laszlo Szalai <lacosjager87@gmail.com>
2012-11-22 11:12:25 +0100

    added gravatar handling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
+# ide irsz vmit akkor az a user_controller viewjai alatt lesz csak elerheto
 module UsersHelper
+	# Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
