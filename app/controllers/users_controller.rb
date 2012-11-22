a56969d2144ec9566db14da2d15859cce24d0aff
Laszlo Szalai <lacosjager87@gmail.com>
2012-11-22 14:20:55 +0100

    successfull registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
 # ez hozza van kapcsolodva a route-okhoz pl show user route az 1 GET keres egy szimbolummal az ":id"-val ami itt is megjelenik
 # a params tomb meg az a kollekcio ami mindig elerunk
-	@user = User.find(params[:id])
+		@user = User.find(params[:id])
+  end
+
+	def create
+# az urlap parameterei a params tombbol kapjuk a feluletrol
+		@user = User.new(params[:user])
+    if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,4 +19,34 @@ describe "UserPages" do
 	  it { should have_selector('h1',    text: user.name) }
 	  it { should have_selector('title', text: user.name) }
 	end
+
+	describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+				## azt hasonlitja ossze h a userek szama voltozik-e, mert lefuttatja elotte meg utanna
+			## a count onnan jon h a user egy activeracordos osztaly es annak van countja alapbol, itt a count csak egy szimbolum es azt hivja meg ugye
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
+
 end
